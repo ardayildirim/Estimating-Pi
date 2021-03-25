17fff946a93f77b204d1875f1cf2c8492dc6ce00
ardayildirim <arda.yildirim158@gmail.com>
2021-03-25 13:59:37 +0300

    added c++ implementation

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+#include <iomanip>
+#include <cmath>
+
+
+using std::cout;
+
+// returns a number between -1 and 1
+double random_number()
+{
+    //rand / randmax is between 0 and 1. Mulptiply by 2 then the interval is (0,2). Subtract 1 and the interval is -1,1.
+    return ( (double) rand() / RAND_MAX) * 2 - 1;
+}
+
+const long TEST_CASE = 1e8;
+
+int main()
+{
+    srand(time(0));
+
+    long successor = 0;
+    for(long i = 0; i < TEST_CASE; i++)
+    {
+        double px = random_number();
+        double py = random_number();
+
+        if(px*px + py*py <= 1)
+            successor++;
+    }
+
+    double estimation = 4.0 * successor / TEST_CASE;
+    cout << "Estimation: " << estimation << "\n";
+
+    cout << std::fixed;
+    cout << std::setprecision(6);
+    cout << "Error is " << abs(M_PI - estimation)/M_PI * 100 <<  " %\n";
+
+    return 0;
+}
